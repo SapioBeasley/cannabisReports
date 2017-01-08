50da2ef8a53815c548ab21246b18cd765b2e0dff
Andreas Beasley <andreas.beasley@gmail.com>
2017-01-07 19:41:24 -0800

    new package

---
+++ src/CannabisReportsServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sapioweb\CannabisReports;
+
+use Illuminate\Support\ServiceProvider;
+
+class CannabisReportsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__.'/routes.php';
+        $this->app->make('Sapioweb\CannabisReports\CannabisDispensaries');
+        $this->app->make('Sapioweb\CannabisReports\CannabisEdibles');
+        $this->app->make('Sapioweb\CannabisReports\CannabisExtracts');
+        $this->app->make('Sapioweb\CannabisReports\CannabisFlowers');
+        $this->app->make('Sapioweb\CannabisReports\CannabisProducers');
+        $this->app->make('Sapioweb\CannabisReports\CannabisProducts');
+        $this->app->make('Sapioweb\CannabisReports\CannabisSeedCompanies');
+        $this->app->make('Sapioweb\CannabisReports\CannabisStrains');
+    }
+}
